639a68b29dbd93f1643316baa6e5433957c8b04b
Colin Cason <colinjcason@gmail.com>
2019-11-02 11:47:54 -0700

    Updates

---
+++ precache-manifest.9537b37c4a23add7b13d9dfae2e0daa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a294d04559104ba0c7ab715d9b7fd2d2",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "ffc10cef1670ed0e43ea",
+    "url": "/clicky-game/static/css/main.3fea6509.chunk.css"
+  },
+  {
+    "revision": "858337689f8457637e8e",
+    "url": "/clicky-game/static/js/2.05bcf8b3.chunk.js"
+  },
+  {
+    "revision": "ffc10cef1670ed0e43ea",
+    "url": "/clicky-game/static/js/main.d71267f8.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
